798118ce11c3aedc31c947c3c5fdabb33c7e6f88
mbhufford <mbhufford@gmail.com>
2026-02-11 17:37:36 -0600

    updated bash

--- course-files/Week_04/blastn.sh
+++ course-files/Week_04/blastn.sh
@@ -16,7 +16,7 @@ module load blast-plus
 
 blastx \
    -db /ptmp/mhufford-lab/Zea_mays.B73_RefGen_v4.pep.all.fa \
-   -query ptmp/mhufford-lab/Zea_maysb73v4.AGPv4.cdna.all.fa \
+   -query /ptmp/mhufford-lab/Zea_maysb73v4.AGPv4.cdna.all.fa \
    -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend qlen sstart send slen evalue frames sgi staxid" \
    -num_threads 8 \
    -out zm_blastx_zea.out
